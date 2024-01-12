9b2752676bd4a69b1f35d4c3c9fe8a1c5ffc5ae2
[Spencer Cook] <spencerrcook@hotmail.com>
2024-01-12 14:17:00 -0700

    fixing connections

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,10 +2,21 @@ const router = require("express").Router();
 const { Candle, Soap } = require("../models");
 
 // route to get home page
+//http://localhost:3001/
 router.get("/", async (req, res) => {
   try {
     //send to homepage later, all currently
     res.status(200).render("all");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/homepage
+router.get("/homepage", async (req, res) => {
+  try {
+    //send to homepage later, all currently
+    res.status(200).render("homePage");
   } catch (err) {
     res.status(500).json(err);
   }
