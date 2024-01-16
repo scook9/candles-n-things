08decbafe8789a2e04329fed428cd0fdd01e6bcf
Halden Kavanagh <haldenkav@gmail.com>
2024-01-16 14:27:58 -0700

    added data for salt scrubs, prices for all products. scrubs now display in the shop page

--- public/js/shop.js
+++ public/js/shop.js
@@ -3,10 +3,13 @@
 const showAll = document.getElementById("showAll");
 const showCandles = document.getElementById("showCandles");
 const showSoaps = document.getElementById("showSoaps");
+const showScrubs = document.getElementById("showScrubs");
 const candles = document.getElementsByClassName("candle");
 const soaps = document.getElementsByClassName("soap");
+const scrubs = document.getElementsByClassName("scrubs");
 const candleHeader = document.getElementById("candleHeader");
 const soapHeader = document.getElementById("soapHeader");
+const scrubHeader = document.getElementById("scrubHeader");
 const candleDisclaimer = document.getElementById("candleDisclaimer");
 const soapDisclaimer = document.getElementById("soapDisclaimer");
 
@@ -15,12 +18,14 @@ const soapDisclaimer = document.getElementById("soapDisclaimer");
 showAll.addEventListener("click", showAllFunc);
 showCandles.addEventListener("click", showCandlesFunc);
 showSoaps.addEventListener("click", showSoapsFunc);
+showScrubs.addEventListener("click", showScrubsFunc);
 
 function showAllFunc() {
   console.log("reached show all function");
 
   candleHeader.classList.add("hide");
   soapHeader.classList.add("hide");
+  scrubHeader.classList.add("hide");
   candleDisclaimer.classList.add("hide");
   soapDisclaimer.classList.add("hide");
 
@@ -30,6 +35,9 @@ function showAllFunc() {
   for (let i = 0; i < candles.length; i++) {
     soaps[i].classList.remove("hide");
   }
+  for (let i = 0; i < scrubs.length; i++) {
+    scrubs[i].classList.remove("hide");
+  }
 }
 
 function showCandlesFunc() {
@@ -37,6 +45,7 @@ function showCandlesFunc() {
 
   candleHeader.classList.remove("hide");
   soapHeader.classList.add("hide");
+  scrubHeader.classList.add("hide");
   candleDisclaimer.classList.remove("hide");
   soapDisclaimer.classList.add("hide");
 
@@ -46,6 +55,9 @@ function showCandlesFunc() {
   for (let i = 0; i < candles.length; i++) {
     soaps[i].classList.add("hide");
   }
+  for (let i = 0; i < scrubs.length; i++) {
+    scrubs[i].classList.add("hide");
+  }
 }
 
 function showSoapsFunc() {
@@ -53,6 +65,7 @@ function showSoapsFunc() {
 
   candleHeader.classList.add("hide");
   soapHeader.classList.remove("hide");
+  scrubHeader.classList.add("hide");
   candleDisclaimer.classList.add("hide");
   soapDisclaimer.classList.remove("hide");
 
@@ -61,5 +74,28 @@ function showSoapsFunc() {
   }
   for (let i = 0; i < candles.length; i++) {
     soaps[i].classList.remove("hide");
+  }
+  for (let i = 0; i < scrubs.length; i++) {
+    scrubs[i].classList.add("hide");
+  }
+}
+
+function showScrubsFunc() {
+  console.log("reached show all Scrubs function");
+
+  candleHeader.classList.add("hide");
+  soapHeader.classList.add("hide");
+  scrubHeader.classList.remove("hide");
+  candleDisclaimer.classList.add("hide");
+  soapDisclaimer.classList.add("hide");
+
+  for (let i = 0; i < candles.length; i++) {
+    candles[i].classList.add("hide");
+  }
+  for (let i = 0; i < candles.length; i++) {
+    soaps[i].classList.add("hide");
+  }
+  for (let i = 0; i < scrubs.length; i++) {
+    scrubs[i].classList.remove("hide");
   }
 }
