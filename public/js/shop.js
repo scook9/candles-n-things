5ece9ba226158b5209f34359f998e203aaeef205
laurabacon <laura_kessinger@yahoo.com>
2024-01-18 17:54:41 -0700

    redo local storage array

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,5 +1,4 @@
-// variable declorations
-
+// Variable declarations
 const showAll = document.getElementById("showAll");
 const showCandles = document.getElementById("showCandles");
 const showSoaps = document.getElementById("showSoaps");
@@ -14,14 +13,12 @@ const candleDisclaimer = document.getElementById("candleDisclaimer");
 const soapDisclaimer = document.getElementById("soapDisclaimer");
 
 const displayEl = document.getElementsByClassName("displayCart");
-
 const cartAdd = document.querySelector("#cartAdd");
-
 const buttonListEl = document.querySelector(".products");
 
 let cartId;
-// Event listeners
 
+// Event listeners
 showAll.addEventListener("click", showAllFunc);
 showCandles.addEventListener("click", showCandlesFunc);
 showSoaps.addEventListener("click", showSoapsFunc);
@@ -29,17 +26,20 @@ showScrubs.addEventListener("click", showScrubsFunc);
 
 buttonListEl.addEventListener("click", function (event) {
   event.preventDefault();
-  cartId = $(event.target).attr("data-id");
+  cartId = event.target.getAttribute("data-id");
   console.log(cartId);
-  localStorage.setItem(cartId, "");
 
-  // addToCart(productId, productName, productPrice);
+  // Retrieve the cart items from local storage or initialize an empty array
+  const cartItems = JSON.parse(localStorage.getItem("cartData")) || [];
+
+  // Add the cart item to the cartItems array
+  cartItems.push({ cartId });
+
+  // Store the updated cart items in local storage
+  localStorage.setItem("cartData", JSON.stringify(cartItems));
 });
 
-// console.log(event.target);
-
-// function to show all products. removes hide class from all descriptions and disclaimers, then removes hide from all products
-
+// Function to show all products. Removes hide class from all descriptions and disclaimers, then removes hide from all products
 function showAllFunc() {
   console.log("reached show all function");
 
@@ -52,16 +52,17 @@ function showAllFunc() {
   for (let i = 0; i < candles.length; i++) {
     candles[i].classList.remove("hide");
   }
+
   for (let i = 0; i < soaps.length; i++) {
     soaps[i].classList.remove("hide");
   }
+
   for (let i = 0; i < scrubs.length; i++) {
     scrubs[i].classList.remove("hide");
   }
 }
 
-// function to remove hide from all candles and the candles description/disclaimer. Adds hide class to everything else
-
+// Function to remove hide from all candles and the candles description/disclaimer. Adds hide class to everything else
 function showCandlesFunc() {
   console.log("reached show all Candles function");
 
@@ -74,16 +75,17 @@ function showCandlesFunc() {
   for (let i = 0; i < candles.length; i++) {
     candles[i].classList.remove("hide");
   }
+
   for (let i = 0; i < soaps.length; i++) {
     soaps[i].classList.add("hide");
   }
+
   for (let i = 0; i < scrubs.length; i++) {
     scrubs[i].classList.add("hide");
   }
 }
 
-// function to remove hide from all soaps and the soaps description/disclaimer. Adds hide class to everything else
-
+// Function to remove hide from all soaps and the soaps description/disclaimer. Adds hide class to everything else
 function showSoapsFunc() {
   console.log("reached show all Soaps function");
 
@@ -96,9 +98,11 @@ function showSoapsFunc() {
   for (let i = 0; i < candles.length; i++) {
     candles[i].classList.add("hide");
   }
+
   for (let i = 0; i < soaps.length; i++) {
     soaps[i].classList.remove("hide");
   }
+
   for (let i = 0; i < scrubs.length; i++) {
     scrubs[i].classList.add("hide");
   }
