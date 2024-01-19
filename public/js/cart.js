d5da79fee74a768116cda7a46da4572cd9ae56eb
Halden Kavanagh <haldenkav@gmail.com>
2024-01-18 17:57:30 -0700

    styling to main.handlebars

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,118 +1,107 @@
-
 const { Products } = require("../../models");
 const cartId = require("./shop");
 
-
 // Constructor function for cart items
 function Item(id, price, count) {
-    this.scent = id;
-    this.price = price;
-    this.count = count;
-  }
-  
-  // Function to retrieve cart data from local storage and render cart items
-  function renderCartItems() {
-    const cartData = JSON.parse(localStorage.getItem("cartData")) || [];
-    const cartContainer = document.getElementById("cartContainer");
-  
-    cartContainer.innerHTML = "";
-  
-    cartData.forEach(function (item) {
-      const cartItem = document.createElement("div");
-      cartItem.classList.add("cart-item");
-      cartItem.innerHTML = `
+  this.scent = id;
+  this.price = price;
+  this.count = count;
+}
+
+// Function to retrieve cart data from local storage and render cart items
+function renderCartItems() {
+  const cartData = JSON.parse(localStorage.getItem("cartData")) || [];
+  const cartContainer = document.getElementById("cartContainer");
+
+  cartContainer.innerHTML = "";
+
+  cartData.forEach(function (item) {
+    const cartItem = document.createElement("div");
+    cartItem.classList.add("cart-item");
+    cartItem.innerHTML = `
         <h3>${item.name}</h3>
         <p>$${item.price}</p>
       `;
-      cartContainer.appendChild(cartItem);
-    });
+    cartContainer.appendChild(cartItem);
+  });
+}
+
+// Call the renderCartItems function to display the cart items on page load
+renderCartItems();
+
+// Object to encapsulate cart functionality
+const myCart = (function () {
+  let createCart = [];
+
+  function getCart() {
+    const storedCart = localStorage.getItem("myCart");
+    if (storedCart) {
+      createCart = JSON.parse(storedCart);
+    }
   }
 
-  
-  // Call the renderCartItems function to display the cart items on page load
-  renderCartItems();
-  
-  // Object to encapsulate cart functionality
-  const myCart = (function () {
-    let createCart = [];
-  
-    function getCart() {
-      const storedCart = localStorage.getItem("myCart");
-      if (storedCart) {
-        createCart = JSON.parse(storedCart);
+  function saveCart() {
+    localStorage.setItem("myCart", JSON.stringify(createCart));
+  }
+
+  const cart = {};
+
+  cart.addItemToCart = function (name, price, count) {
+    for (let item of createCart) {
+      if (item.name === name) {
+        item.count++;
+        saveCart();
+        return;
       }
     }
-  
-    function saveCart() {
-      localStorage.setItem("myCart", JSON.stringify(createCart));
+    const newItem = new Item(name, price, count);
+    createCart.push(newItem);
+    saveCart();
+  };
+
+  cart.setCountForItem = function (name, count) {
+    for (let item of createCart) {
+      if (item.name === name) {
+        item.count = count;
+        break;
+      }
     }
-  
-    const cart = {};
-  
-    cart.addItemToCart = function (name, price, count) {
-      for (let item of createCart) {
-        if (item.name === name) {
-          item.count++;
-          saveCart();
-          return;
+  };
+
+  cart.totalCart = function () {
+    return createCart.reduce(
+      (total, item) => total + item.price * item.count,
+      0
+    );
+  };
+
+  cart.listCart = function () {
+    return createCart.map((item) => {
+      const itemCopy = { ...item };
+      itemCopy.total = (item.price * item.count).toFixed(2);
+      return itemCopy;
+    });
+  };
+
+  cart.removeItemFromCart = function (name) {
+    for (let i = 0; i < createCart.length; i++) {
+      if (createCart[i].name === name) {
+        createCart[i].count--;
+        if (createCart[i].count === 0) {
+          createCart.splice(i, 1);
         }
+        break;
       }
-      const newItem = new Item(name, price, count);
-      createCart.push(newItem);
-      saveCart();
-    };
-  
-    cart.setCountForItem = function (name, count) {
-      for (let item of createCart) {
-        if (item.name === name) {
-          item.count = count;
-          break;
-        }
-      }
-    };
-  
-    cart.totalCart = function () {
-      return createCart.reduce((total, item) => total + item.price * item.count, 0);
-    };
-  
-    cart.listCart = function () {
-      return createCart.map((item) => {
-        const itemCopy = { ...item };
-        itemCopy.total = (item.price * item.count).toFixed(2);
-        return itemCopy;
-      });
-    };
-  
-    cart.removeItemFromCart = function (name) {
-      for (let i = 0; i < createCart.length; i++) {
-        if (createCart[i].name === name) {
-          createCart[i].count--;
-          if (createCart[i].count === 0) {
-            createCart.splice(i, 1);
-          }
-          break;
-        }
-      }
-      saveCart();
-    };
-  
-    getCart();
-  
-    return cart;
-  })();
-  
-  module.exports = myCart;
+    }
+    saveCart();
+  };
 
+  getCart();
 
+  return cart;
+})();
 
-
-
-
-
-
-
-
-
+module.exports = myCart;
 
 // const myCart = (function () {
 //   createCart = [];
@@ -173,11 +162,6 @@ function Item(id, price, count) {
 // })
 // //execute 'myCart' function
 
-
-
-
-
-
 // //need to attach to buttons on the cart page to remove a single item
 // cart.removeItemFromCart = function (name) {
 //     for (var item in createCart) {
@@ -192,19 +176,13 @@ function Item(id, price, count) {
 //     saveCart();
 //   };
 
-
-
-
 // //need to attach to buttons on the cart page to remove all items
 // function clearCart() {
 //     createCart = [];
 //     saveCart();
 // };
 
-
-
 // //need to add   id="add-to-cart-btn"  to the buttons on the cards on the shop
 // document.getElementById("add-to-cart-btn").addEventListener("click", function() {
 //     myCart.addItemToCart("Product A", 10, 1);
 //   });
-  
